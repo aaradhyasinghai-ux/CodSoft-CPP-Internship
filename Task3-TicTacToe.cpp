a52ecd0fc381974ea13d6cd014cbf421cdbe034d
Aaradhya Singhai <aaradhya.25mei10078@vitbhopal.ac.in>
2026-01-09 21:29:13 +0530

    Add files via upload

---
+++ Task3-TicTacToe.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3];
+char currentPlayer;
+
+// Initialize the board
+void resetBoard() {
+    char ch = '1';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ch++;
+        }
+    }
+    currentPlayer = 'X';
+}
+
+// Display the board
+void displayBoard() {
+    cout << "\n";
+    for (int i = 0; i < 3; i++) {
+        cout << " ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << endl;
+        if (i < 2) cout << "---+---+---\n";
+    }
+    cout << endl;
+}
+
+// Switch player
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+// Check win condition
+bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == currentPlayer &&
+            board[i][1] == currentPlayer &&
+            board[i][2] == currentPlayer)
+            return true;
+
+        if (board[0][i] == currentPlayer &&
+            board[1][i] == currentPlayer &&
+            board[2][i] == currentPlayer)
+            return true;
+    }
+
+    if (board[0][0] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][2] == currentPlayer)
+        return true;
+
+    if (board[0][2] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][0] == currentPlayer)
+        return true;
+
+    return false;
+}
+
+// Check draw condition
+bool checkDraw() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+    return true;
+}
+
+// Player move
+void playerMove() {
+    int choice;
+    cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+    cin >> choice;
+
+    int row = (choice - 1) / 3;
+    int col = (choice - 1) % 3;
+
+    if (choice < 1 || choice > 9 ||
+        board[row][col] == 'X' ||
+        board[row][col] == 'O') {
+        cout << "Invalid move! Try again.\n";
+        playerMove();
+    } else {
+        board[row][col] = currentPlayer;
+    }
+}
+
+int main() {
+    char playAgain;
+
+    do {
+        resetBoard();
+        bool gameOver = false;
+
+        while (!gameOver) {
+            displayBoard();
+            playerMove();
+
+            if (checkWin()) {
+                displayBoard();
+                cout << "Hooray! Player " << currentPlayer << " wins!\n";
+                gameOver = true;
+            } else if (checkDraw()) {
+                displayBoard();
+                cout << "Game is a draw!\n";
+                gameOver = true;
+            } else {
+                switchPlayer();
+            }
+        }
+
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thanks for playing Tic-Tac-Toe!\n";
+    return 0;
+}
